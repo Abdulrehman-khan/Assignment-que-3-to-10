d91a22587d707b6ac9363767cbbc06da2e04bba6
Abdulrehman <khankakarabdulrehman4@gmail.com>
2023-08-08 20:37:52 -0700

    assignment answer

---
+++ index.ts
@@ -0,0 +1,8 @@
+//  Titel Case
+let q3: string = "ErIc";
+console.log(
+    `Hello ${q3.toLowerCase()}, would you like to learn some Python today?`
+  );
+  console.log(
+    `Hello ${q3.toUpperCase()}, would you like to learn some Python today?`
+  );
